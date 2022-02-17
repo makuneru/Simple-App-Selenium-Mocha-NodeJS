622f25eade8f448f40b13d229a772a4490910ef1
Mac Cayanan <macnel.cayanan@oracle.com>
2022-02-17 11:34:54 +0800

    first commit

--- test/basic-login-tests.js
+++ test/basic-login-tests.js
@@ -10,9 +10,6 @@ describe('Simple App', function() {
   describe('for anonymous user', function () {
     it('home page shows login link', async function () {
       assert.fail("test is not yet implemented");
-      // await driver.get("http://localhost:3000")
-      // const alert = await (await driver.findElement(By.xpath("//a[@href='/login']"))).getText()
-      // assert.equal(alert, "login");
     });
   });
 });
